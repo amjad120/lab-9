7698d9c9c4f99ad7c5e11383fd5b023913bbc48e
Amjad Alinizi <129985471+amjad120@users.noreply.github.com>
2024-12-03 05:15:14 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './/logo.svg';
+import './/App.css';
+import Header from './/components//Header';
+import Footer from './/components//Footer';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import About from './/components//About';
+import Home from './/components//Home';
+import Recipe from './/components//Recipe';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <div className="Container">
+      <Header/>
+    <main>
+    <Routes>
+
+      <Route path='/' element={<Home/>}/>
+      <Route path='/About' element={<About/>}/>
+      <Route path='/recipe/:id' element={<Recipe/>}/>
+    
+    </Routes>
+    </main>
+    <Footer/>
     </div>
+    </BrowserRouter>
   );
 }
 
